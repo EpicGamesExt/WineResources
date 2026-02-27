e003caa88d52681541688b2495e9123e890f1998
Adam Rehn <adam@adamrehn.com>
2026-02-27 16:52:14 +1000

    Use a Python virtual environment when running build.py under Linux

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,12 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 set -ex
 
+# Create a Python virtual environment to hold the required dependencies
+rm -rf "$SCRIPT_DIR/.venv"
+python3 -m venv "$SCRIPT_DIR/.venv"
+
 # Install the Python packages that the build script depends upon
-python3 -m pip install -r "$SCRIPT_DIR/requirements.txt"
+"$SCRIPT_DIR/.venv/bin/python3" -m pip install -r "$SCRIPT_DIR/requirements.txt"
 
 # Run the build script, propagating any command-line arguments
-python3 "$SCRIPT_DIR/build.py" "$@"
+"$SCRIPT_DIR/.venv/bin/python3" "$SCRIPT_DIR/build.py" "$@"
